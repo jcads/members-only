44f7f34a9d37fa6dfa913506e5ff5cf78eda0633
rafael <dragonwood0@protonmail.com>
2020-11-15 23:16:00 +0800

    Added the messages on the home page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,14 +1,33 @@
+const User = require("../models/user");
+const Post = require("../models/post");
+const {body, validationResult, check} = require("express-validator");
+
 exports.join_get = function(req, res, next) {
     res.render("join_form", {title: "Join"});
 }
 
-exports.join_post = function(req, res, next) {
-    console.log(req.body);
-}
+exports.join_post = [
+    body("secretCode").escape(),
+    body("secretCode").custom((value, {req}) => {
+      if (req.body.secretCode === "test") return true;
+      throw new Error("Wrong code");
+    }),
+
+    (req, res, next) => {
+        const errs = validationResult(req);
+        if (errs.isEmpty()) {
+            User.findByIdAndUpdate(req.user._id, {membershipStat: "Member"}, (err, updatedUser) => {
+                if (err) return next(err);
+                res.redirect("/home");
+            });
+        } else {
+            res.render("join_form", {title: "Join", errors: errs.array()});
+        }
+}]; 
 
 exports.home_get = function(req, res, next) {
-    if (req.isAuthenticated()) {
-       res.render("home");
-    }
-    else res.redirect("/login");
-}; 
+    Post.find({}).populate("author").exec((err, posts) => {
+        if (err) return next(err);
+        res.render("home", {title: "Clubhouse | Home", posts: posts});
+    });
+};
